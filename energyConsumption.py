9220d2475be784377b5dc7f3b598a032f8a9c254
Brad-Davy <69325766+Brad-Davy@users.noreply.github.com>
2024-06-17 11:22:27 +0100

    Refactered the code so everything runs through the returnTotalEnergy() function, much cleaner and easier to run new cases

--- energyConsumption.py
+++ energyConsumption.py
@@ -16,6 +16,16 @@ LEAKAGE_POWER = 0.1
 GOPS_PER_WATT = 40
 MY_PARAMETERS = {'BW' : 50, 'Ant' : 2, 'M' : 3/4, 'R' : 2, 'dt' : 1, 'df' : 100}
 
+# =============================================================================
+# Parameters descritions:
+#     BW = Bandwidth -> Default value: 10.
+#     Ant = number of antennas -> Default value: 2.
+#     M = Modulation -> Default value: 6.
+#     R = Coding rate -> Default value: 5/6.
+#     dt = Time-domain duty-cycling -> Default value: 1.
+#     df = Frequency-domain duty-cycling -> Default value: 1.
+# =============================================================================
+    
 
 BASE_BAND_COMPONENTS = {'DPD' : True, 'Filter' : True, 'OFDM' : True, 'FD' : True, 
                       'FD_NL' : True, 'FEC' : True, 'CPU' : True}
@@ -315,7 +325,8 @@ def RFTransmitterPower(parameters = MY_PARAMETERS):
     return IQPower + ATTENPower + BUFFPOwer + FVCPower + FECVPower + FMPower + CLKPower + DCPower + ADCPower
 
 def RFReceiverPower(parameters = MY_PARAMETERS):
-
+    
+    print(parameters)
     powerOfLNA1 = returnTotalRFComponentPower(component = 'LNA1', current_values=[parameters['BW'], parameters['Ant'], parameters['M'], parameters['R'], parameters['dt'], parameters['df']], transmitter = False)
     powerATTEN = returnTotalRFComponentPower(component = 'ATTEN', current_values=[parameters['BW'], parameters['Ant'], parameters['M'], parameters['R'], parameters['dt'], parameters['df']], transmitter = False)
     powerOfLNA2 = returnTotalRFComponentPower(component = 'LNA2', current_values=[parameters['BW'], parameters['Ant'], parameters['M'], parameters['R'], parameters['dt'], parameters['df']], transmitter = False)
@@ -329,13 +340,16 @@ def RFReceiverPower(parameters = MY_PARAMETERS):
 def returnPAPower(parameters = MY_PARAMETERS):
     load = parameters['dt']
     total_power_PA = [float(i) for i in open('powerAmplifierLoad.txt', 'r').read().split('\n')]
-    return total_power_PA[load-1]
+    return total_power_PA[load]
 
 def returnOverHead(BBPower, RFPower, PAPower):
-    coolingOH = 0.1
+    coolingOH = 0.05
     DC_DCOH = 0.05
     AC_DCOH = 0.1
-    return (BBPower + RFPower + PAPower) * ((1 + coolingOH)*(1+DC_DCOH)*(1 + AC_DCOH) - 1)
+    factor =  (((1 + coolingOH) * (1 + DC_DCOH) * (1 + AC_DCOH)) - 1)
+    print(factor)
+    print(BBPower, RFPower, PAPower)
+    return (BBPower + RFPower + PAPower) * factor
     
 
 def returnTotalPower(parameters = MY_PARAMETERS):
@@ -351,189 +365,11 @@ def returnTotalPower(parameters = MY_PARAMETERS):
     None.
 
     """
-    
-    baseBandPower = baseBandUploadPower(parameters = MY_PARAMETERS) + baseBandDownloadPower(parameters = MY_PARAMETERS)
-    RFPower = RFTransmitterPower(parameters = MY_PARAMETERS) + RFReceiverPower(parameters = MY_PARAMETERS)
-    PAPower = returnPAPower(parameters = MY_PARAMETERS)
+
+    baseBandPower = baseBandUploadPower(parameters = parameters) + baseBandDownloadPower(parameters = parameters)
+    RFPower = (RFTransmitterPower(parameters = parameters) + RFReceiverPower(parameters = parameters)) / 1000
+    PAPower = returnPAPower(parameters = parameters)
     OHPower = returnOverHead(baseBandPower, RFPower, PAPower)
-    return baseBandPower, RFPower/1000, PAPower, OHPower
     
-print(returnTotalPower())
-
-# =============================================================================
-# Some code to reproduce the figure 2 from the Desset et al paper
-# =============================================================================
-
-def returnFigure2EnergyDownlink(LOAD):
-    """
-    
-
-    Parameters
-    ----------
-    LOAD : TYPE
-        DESCRIPTION.
-
-    Returns
-    -------
-    TYPE
-        DESCRIPTION.
-
-    """
-    
-    powerOfFD = []
-    powerOfCPU = []
-    powerOfFEC = []
-    powerOfFD_NL = []
-    powerOfOFDM = []
-    powerOfFILETER = []
-    powerOfDPD = []
-    
-    for l in LOAD:
-        
-        powerOfFD.append(returnTotalBBComponentPower(component = 'FD', current_values=[MY_PARAMETERS['BW'], MY_PARAMETERS['Ant'], MY_PARAMETERS['M'], MY_PARAMETERS['R'], MY_PARAMETERS['dt'], l]))
-        powerOfCPU.append(returnTotalBBComponentPower(component = 'CPU', current_values=[MY_PARAMETERS['BW'], MY_PARAMETERS['Ant'], MY_PARAMETERS['M'], MY_PARAMETERS['R'], MY_PARAMETERS['dt'], l]))
-        powerOfFEC.append(returnTotalBBComponentPower(component = 'FEC', current_values=[MY_PARAMETERS['BW'], MY_PARAMETERS['Ant'], MY_PARAMETERS['M'], MY_PARAMETERS['R'], MY_PARAMETERS['dt'], l]))
-        powerOfFD_NL.append(returnTotalBBComponentPower(component = 'FD_NL', current_values=[MY_PARAMETERS['BW'], MY_PARAMETERS['Ant'], MY_PARAMETERS['M'], MY_PARAMETERS['R'], MY_PARAMETERS['dt'], l]))
-        powerOfOFDM.append(returnTotalBBComponentPower(component = 'OFDM', current_values=[MY_PARAMETERS['BW'], MY_PARAMETERS['Ant'], MY_PARAMETERS['M'], MY_PARAMETERS['R'], MY_PARAMETERS['dt'], l]))
-        powerOfFILETER.append(returnTotalBBComponentPower(component = 'FILTER', current_values=[MY_PARAMETERS['BW'], MY_PARAMETERS['Ant'], MY_PARAMETERS['M'], MY_PARAMETERS['R'], MY_PARAMETERS['dt'], l]))
-        powerOfDPD.append(returnTotalBBComponentPower(component = 'DPD', current_values=[MY_PARAMETERS['BW'], MY_PARAMETERS['Ant'], MY_PARAMETERS['M'], MY_PARAMETERS['R'], MY_PARAMETERS['dt'], l]))
-        
-    return np.array(powerOfFD), np.array(powerOfCPU), np.array(powerOfFEC), np.array(powerOfFD_NL), np.array(powerOfOFDM), np.array(powerOfFILETER), np.array(powerOfDPD)
-    
-def returnFigure2EnergyUplink(LOAD):
-    """
-    
-
-    Parameters
-    ----------
-    LOAD : TYPE
-        DESCRIPTION.
-
-    Returns
-    -------
-    TYPE
-        DESCRIPTION.
-
-    """
-    
-    powerOfFD = []
-    powerOfCPU = []
-    powerOfFEC = []
-    powerOfFD_NL = []
-    powerOfOFDM = []
-    powerOfFILETER = []
-    
-    for l in LOAD:
-        
-        powerOfFD.append(returnTotalBBComponentPower(component = 'FD', current_values=[MY_PARAMETERS['BW'], MY_PARAMETERS['Ant'], MY_PARAMETERS['M'], MY_PARAMETERS['R'], MY_PARAMETERS['dt'], l], download=False))
-        powerOfCPU.append(returnTotalBBComponentPower(component = 'CPU', current_values=[MY_PARAMETERS['BW'], MY_PARAMETERS['Ant'], MY_PARAMETERS['M'], MY_PARAMETERS['R'], MY_PARAMETERS['dt'], l], download=False))
-        powerOfFEC.append(returnTotalBBComponentPower(component = 'FEC', current_values=[MY_PARAMETERS['BW'], MY_PARAMETERS['Ant'], MY_PARAMETERS['M'], MY_PARAMETERS['R'], MY_PARAMETERS['dt'], l], download=False))
-        powerOfFD_NL.append(returnTotalBBComponentPower(component = 'FD_NL', current_values=[MY_PARAMETERS['BW'], MY_PARAMETERS['Ant'], MY_PARAMETERS['M'], MY_PARAMETERS['R'], MY_PARAMETERS['dt'], l], download=False))
-        powerOfOFDM.append(returnTotalBBComponentPower(component = 'OFDM', current_values=[MY_PARAMETERS['BW'], MY_PARAMETERS['Ant'], MY_PARAMETERS['M'], MY_PARAMETERS['R'], MY_PARAMETERS['dt'], l], download=False))
-        powerOfFILETER.append(returnTotalBBComponentPower(component = 'FILTER', current_values=[MY_PARAMETERS['BW'], MY_PARAMETERS['Ant'], MY_PARAMETERS['M'], MY_PARAMETERS['R'], MY_PARAMETERS['dt'], l], download=False))
-    
-    return np.array(powerOfFD), np.array(powerOfCPU), np.array(powerOfFEC), np.array(powerOfFD_NL), np.array(powerOfOFDM), np.array(powerOfFILETER)
-    
-def returnFigure2RFTransmitterEnergy(LOAD):
-    """
-    
-
-    Parameters
-    ----------
-    FREQUENCY_MAP : TYPE
-        DESCRIPTION.
-
-    Returns
-    -------
-    TYPE
-        DESCRIPTION.
-
-    """
-    
-    powerOfIQ = []
-    powerOfATTEN = []
-    powerOfBUFF = []
-    powerOfFVC = []
-    powerOfOFEVC = []
-    powerOfFM = []
-    powerOfCLK = []
-    powerOfDC = []
-    powerOfADC = []
-    
-    for l in LOAD:
-        
-        powerOfIQ.append(returnTotalRFComponentPower(component = 'IQMOD', current_values=[MY_PARAMETERS['BW'], MY_PARAMETERS['Ant'], MY_PARAMETERS['M'], MY_PARAMETERS['R'], MY_PARAMETERS['dt'], l]))
-        powerOfATTEN.append(returnTotalRFComponentPower(component = 'ATTEN', current_values=[MY_PARAMETERS['BW'], MY_PARAMETERS['Ant'], MY_PARAMETERS['M'], MY_PARAMETERS['R'], MY_PARAMETERS['dt'], l]))
-        powerOfBUFF.append(returnTotalRFComponentPower(component = 'BUFFER', current_values=[MY_PARAMETERS['BW'], MY_PARAMETERS['Ant'], MY_PARAMETERS['M'], MY_PARAMETERS['R'], MY_PARAMETERS['dt'], l]))
-        powerOfFVC.append(returnTotalRFComponentPower(component = 'FOWARD_VOLTAGE_CONTROL', current_values=[MY_PARAMETERS['BW'], MY_PARAMETERS['Ant'], MY_PARAMETERS['M'], MY_PARAMETERS['R'], MY_PARAMETERS['dt'], l]))
-        powerOfOFEVC.append(returnTotalRFComponentPower(component = 'FEEDBACK_VOLTAGE_CONROL', current_values=[MY_PARAMETERS['BW'], MY_PARAMETERS['Ant'], MY_PARAMETERS['M'], MY_PARAMETERS['R'], MY_PARAMETERS['dt'], l]))
-        powerOfFM.append(returnTotalRFComponentPower(component = 'FEEDBACK_MIXER', current_values=[MY_PARAMETERS['BW'], MY_PARAMETERS['Ant'], MY_PARAMETERS['M'], MY_PARAMETERS['R'], MY_PARAMETERS['dt'], l]))
-        powerOfCLK.append(returnTotalRFComponentPower(component = 'CLOCK', current_values=[MY_PARAMETERS['BW'], MY_PARAMETERS['Ant'], MY_PARAMETERS['M'], MY_PARAMETERS['R'], MY_PARAMETERS['dt'], l]))
-        powerOfDC.append(returnTotalRFComponentPower(component = 'DAC_CONCVERTER', current_values=[MY_PARAMETERS['BW'], MY_PARAMETERS['Ant'], MY_PARAMETERS['M'], MY_PARAMETERS['R'], MY_PARAMETERS['dt'],l]))
-        powerOfADC.append(returnTotalRFComponentPower(component = 'ADC_CONTROL', current_values=[MY_PARAMETERS['BW'], MY_PARAMETERS['Ant'], MY_PARAMETERS['M'], MY_PARAMETERS['R'], MY_PARAMETERS['dt'], l]))
-               
-        
-    return np.array(powerOfIQ), np.array(powerOfATTEN), np.array(powerOfBUFF), np.array(powerOfFVC), np.array(powerOfOFEVC), np.array(powerOfFM), np.array(powerOfCLK), np.array(powerOfDC), np.array(powerOfADC)
-    
-
-def returnFigure2RFReceiverEnergy(LOAD):
-    """
-    
-
-    Parameters
-    ----------
-    FREQUENCY_MAP : TYPE
-        DESCRIPTION.
-
-    Returns
-    -------
-    TYPE
-        DESCRIPTION.
-
-    """
-
-    powerOfLNA1 = []
-    powerOfATTEN = []
-    powerOfLNA2 = []
-    powerOfDUAL_MIXER = []
-    powerOfOVGA = []
-    powerOfCLKGEN = []
-    powerOfADC = []
-    
-    for l in LOAD:
-        
-        powerOfLNA1.append(returnTotalRFComponentPower(component = 'LNA1', current_values=[MY_PARAMETERS['BW'], MY_PARAMETERS['Ant'], MY_PARAMETERS['M'], MY_PARAMETERS['R'], MY_PARAMETERS['dt'], l], transmitter = False))
-        powerOfATTEN.append(returnTotalRFComponentPower(component = 'ATTEN', current_values=[MY_PARAMETERS['BW'], MY_PARAMETERS['Ant'], MY_PARAMETERS['M'], MY_PARAMETERS['R'], MY_PARAMETERS['dt'], l], transmitter = False))
-        powerOfLNA2.append(returnTotalRFComponentPower(component = 'LNA2', current_values=[MY_PARAMETERS['BW'], MY_PARAMETERS['Ant'], MY_PARAMETERS['M'], MY_PARAMETERS['R'], MY_PARAMETERS['dt'], l], transmitter = False))
-        powerOfDUAL_MIXER.append(returnTotalRFComponentPower(component = 'DUAL_MIXER', current_values=[MY_PARAMETERS['BW'], MY_PARAMETERS['Ant'], MY_PARAMETERS['M'], MY_PARAMETERS['R'], MY_PARAMETERS['dt'], l], transmitter = False))
-        powerOfOVGA.append(returnTotalRFComponentPower(component = 'VGA', current_values=[MY_PARAMETERS['BW'], MY_PARAMETERS['Ant'], MY_PARAMETERS['M'], MY_PARAMETERS['R'], MY_PARAMETERS['dt'], l], transmitter = False))
-        powerOfCLKGEN.append(returnTotalRFComponentPower(component = 'CLOCK_GEN', current_values=[MY_PARAMETERS['BW'], MY_PARAMETERS['Ant'], MY_PARAMETERS['M'], MY_PARAMETERS['R'], MY_PARAMETERS['dt'], l], transmitter = False))
-        powerOfADC.append(returnTotalRFComponentPower(component = 'ADC', current_values=[MY_PARAMETERS['BW'], MY_PARAMETERS['Ant'], MY_PARAMETERS['M'], MY_PARAMETERS['R'], MY_PARAMETERS['dt'], l], transmitter = False))
-               
-    return np.array(powerOfLNA1), np.array(powerOfATTEN), np.array(powerOfLNA2), np.array(powerOfDUAL_MIXER), np.array(powerOfOVGA), np.array(powerOfCLKGEN), np.array(powerOfADC)
-
-
-def returnPA(LOAD):
-    numberOfAntennas = MY_PARAMETERS['Ant']
-    maximumPower = 390 * numberOfAntennas
-    minimumPower = 11 * numberOfAntennas
-    return np.linspace(minimumPower, maximumPower, len(LOAD))
-
-
-def returnOverHeadHelper(BBPower, RFPower, PAPower):
-    coolingOH = 0.1
-    DC_DCOH = 0.05
-    AC_DCOH = 0.1
-    return (BBPower + RFPower + PAPower) * ((1 + coolingOH)*(1+DC_DCOH)*(1 + AC_DCOH) - 1)
-    
-def returnOverHead(BBPower, RFPower, PAPower):
-    overHead = []
-    for idx, value in enumerate(BBPower):
-        overHead.append(returnOverHeadHelper(value, RFPower[idx], PAPower[idx]))
-        
-    return np.array(overHead)
-    
-    
-    
-    
+    return baseBandPower, RFPower, PAPower, OHPower
     
--- plotting.py
+++ plotting.py
@@ -20,129 +20,28 @@ plt.rcParams["figure.autolayout"] = True
 cm = 1/2.54  # centimeters in inches
 
 FREQUENCY_MAP = np.linspace(0, 100, 100)
-LOAD = np.linspace(0,100,100)
 
-def powerOfBasebandDownLink(total = False):   
-    
-    powerOfFD, powerOfCPU, powerOfFEC, powerOfFD_NL, powerOfOFDM, powerOfFILETER, powerOfDPD = returnBaseBandDowlinkEnergy(FREQUENCY_MAP)
 
-    if total == False:
-        fig = plt.figure(figsize = (15*cm, 7.5*cm))
-        plt.ylabel('Power / W')
-        plt.xlabel('Frequency / MHz')
-        plt.plot(FREQUENCY_MAP, powerOfFD, color=CB91_Blue, label = 'FD')
-        plt.plot(FREQUENCY_MAP, powerOfCPU, color=CB91_Green, label = 'CPU')
-        plt.plot(FREQUENCY_MAP, powerOfFEC, color=CB91_Amber, label = 'FEC')
-        plt.plot(FREQUENCY_MAP, powerOfFD_NL, color=CB91_Pink, label = 'FD (NL)')
-        plt.plot(FREQUENCY_MAP, powerOfOFDM, color=CB91_Purple, label = 'OFDM')
-        plt.plot(FREQUENCY_MAP, powerOfFILETER, color=CB91_Violet, label = 'Filter')
-        plt.plot(FREQUENCY_MAP, powerOfDPD, color='gray', label = 'DPD')
-        plt.legend(ncol = 3, frameon=False)
-        plt.savefig('img/componentBaseBandPowerDownlink.svg', dpi=500)
-
-    else:
-        fig = plt.figure(figsize = (15*cm, 7.5*cm))
-        plt.ylabel('Power / W')
-        plt.xlabel('Frequency / MHz')
-        total_power = np.array(powerOfFD) + np.array(powerOfCPU) + np.array(powerOfFEC) + np.array(powerOfFD_NL) + np.array(powerOfOFDM) + np.array(powerOfFILETER) + np.array(powerOfDPD)
-        plt.plot(FREQUENCY_MAP, total_power + calculateLeakagePower(total_power), color='black')
-        plt.savefig('img/totalBaseBandPowerDownlink.svg', dpi=500)
-        
-        
-def powerOfBasebandUpLink(total = False):    
-    
-    powerOfFD, powerOfCPU, powerOfFEC, powerOfFD_NL, powerOfOFDM, powerOfFILETER = returnBaseBandUplinkEnergy(FREQUENCY_MAP)
-
-    if total == False:
-        fig = plt.figure(figsize = (15*cm, 7.5*cm))
-        plt.ylabel('Power / W')
-        plt.xlabel('Frequency / MHz')
-        plt.plot(FREQUENCY_MAP, powerOfFD, color=CB91_Blue, label = 'FD')
-        plt.plot(FREQUENCY_MAP, powerOfCPU, color=CB91_Green, label = 'CPU')
-        plt.plot(FREQUENCY_MAP, powerOfFEC, color=CB91_Amber, label = 'FEC')
-        plt.plot(FREQUENCY_MAP, powerOfFD_NL, color=CB91_Pink, label = 'FD (NL)')
-        plt.plot(FREQUENCY_MAP, powerOfOFDM, color=CB91_Purple, label = 'OFDM')
-        plt.plot(FREQUENCY_MAP, powerOfFILETER, color=CB91_Violet, label = 'Filter')
-        plt.legend(ncol = 3, frameon=False)
-        plt.savefig('img/componentBaseBandPowerUplink.svg', dpi=500)
-
-    else:
-        fig = plt.figure(figsize = (15*cm, 7.5*cm))
-        plt.ylabel('Power / W')
-        plt.xlabel('Frequency / MHz')
-        total_power = np.array(powerOfFD) + np.array(powerOfCPU) + np.array(powerOfFEC) + np.array(powerOfFD_NL) + np.array(powerOfOFDM) + np.array(powerOfFILETER)
-        plt.plot(FREQUENCY_MAP, total_power + calculateLeakagePower(total_power), color='black')
-        plt.savefig('img/totalBaseBandPowerUplink.svg', dpi=500)
     
 def totalPowerBaseBand():
     fig = plt.figure(figsize = (15*cm, 7.5*cm))
-    powerOfFDUp, powerOfCPUUp, powerOfFECUp, powerOfFD_NLUp, powerOfOFDMUp, powerOfFILETERUp = returnBaseBandUplinkEnergy(FREQUENCY_MAP)
-    powerOfFDDown, powerOfCPUDown, powerOfFECDown, powerOfFD_NLDown, powerOfOFDMDown, powerOfFILETERDown, powerOfDPDDown = returnBaseBandDowlinkEnergy(FREQUENCY_MAP)
-    total_power_up = powerOfFDUp + powerOfCPUUp + powerOfFECUp + powerOfFD_NLUp + powerOfOFDMUp + powerOfFILETERUp 
-    total_power_down = powerOfFDDown + powerOfCPUDown + powerOfFECDown + powerOfFD_NLDown + powerOfOFDMDown + powerOfFILETERDown + powerOfDPDDown 
-    total_power = total_power_up + total_power_down
+    total_power = []
+
+    for f in FREQUENCY_MAP:
+        parameters = {'BW' : f, 'Ant' : 4, 'M' : 3/4, 'R' : 2, 'dt' : 100, 'df' : 100}
+        total_power.append(np.sum(returnTotalPower(parameters)))
+        
+    total_power = np.array(total_power)
+    
+    
+    
     plt.plot(FREQUENCY_MAP, total_power + calculateLeakagePower(total_power), color='black')
     plt.ylabel('Power / W')
     plt.xlabel('Frequency / MHz')
     plt.savefig('img/totalBaseBandPower.svg', dpi=500)
     
-def powerOfRFTransmitter(total = False):    
     
-    powerOfIQ, powerOfATTEN, powerOfBUFF, powerOfFVC, powerOfOFEVC, powerOfFM, powerOfCLK, powerOfDC, powerOfADC = returnRFTransmitterEnergy(FREQUENCY_MAP) 
-   
-
-    if total == False:
-        fig = plt.figure(figsize = (15*cm, 7.5*cm))
-        plt.ylabel('Power / W')
-        plt.xlabel('Frequency / MHz')
-        plt.plot(FREQUENCY_MAP, powerOfIQ / 1000, color=CB91_Blue, label = 'FD')
-        plt.plot(FREQUENCY_MAP, powerOfATTEN / 1000, color=CB91_Green, label = 'CPU')
-        plt.plot(FREQUENCY_MAP, powerOfBUFF/ 1000, color=CB91_Amber, label = 'FEC')
-        plt.plot(FREQUENCY_MAP, powerOfFVC/ 1000, color=CB91_Pink, label = 'FD (NL)')
-        plt.plot(FREQUENCY_MAP, powerOfOFEVC/ 1000, color=CB91_Purple, label = 'OFDM')
-        plt.plot(FREQUENCY_MAP, powerOfFM/ 1000, color=CB91_Violet, label = 'Filter')
-        plt.plot(FREQUENCY_MAP, powerOfCLK/ 1000, color='orange', label = 'Filter')
-        plt.plot(FREQUENCY_MAP, powerOfDC/ 1000, color='orange', label = 'Filter')
-        plt.plot(FREQUENCY_MAP, powerOfADC / 1000, color='orange', label = 'Filter')
-        plt.legend(ncol = 3, frameon=False)
-        plt.savefig('img/componentBaseBandPowerUplink.svg', dpi=500)
-
-    else:
-        fig = plt.figure(figsize = (15*cm, 7.5*cm))
-        plt.ylabel('Power / W')
-        plt.xlabel('Frequency / MHz')
-        total_power = (np.array(powerOfIQ) + np.array(powerOfATTEN) + np.array(powerOfBUFF) + np.array(powerOfFVC) + np.array(powerOfOFEVC) + np.array(powerOfFM) + np.array(powerOfCLK) + np.array(powerOfDC) + np.array(powerOfADC)) / 1000
-        plt.plot(FREQUENCY_MAP, total_power + calculateLeakagePower(total_power), color='black')
-        plt.savefig('img/totalBaseBandPowerUplink.svg', dpi=500)
-
-def powerOfRFReceiver(total = False):    
-    
-    powerOfIQ, powerOfATTEN, powerOfBUFF, powerOfFVC, powerOfOFEVC, powerOfFM, powerOfCLK, powerOfDC, powerOfADC = returnRFEnergy(FREQUENCY_MAP) 
-   
-
-    if total == False:
-        fig = plt.figure(figsize = (15*cm, 7.5*cm))
-        plt.ylabel('Power / W')
-        plt.xlabel('Frequency / MHz')
-        plt.plot(FREQUENCY_MAP, powerOfIQ / 1000, color=CB91_Blue, label = 'FD')
-        plt.plot(FREQUENCY_MAP, powerOfATTEN / 1000, color=CB91_Green, label = 'CPU')
-        plt.plot(FREQUENCY_MAP, powerOfBUFF/ 1000, color=CB91_Amber, label = 'FEC')
-        plt.plot(FREQUENCY_MAP, powerOfFVC/ 1000, color=CB91_Pink, label = 'FD (NL)')
-        plt.plot(FREQUENCY_MAP, powerOfOFEVC/ 1000, color=CB91_Purple, label = 'OFDM')
-        plt.plot(FREQUENCY_MAP, powerOfFM/ 1000, color=CB91_Violet, label = 'Filter')
-        plt.plot(FREQUENCY_MAP, powerOfCLK/ 1000, color='orange', label = 'Filter')
-        plt.plot(FREQUENCY_MAP, powerOfDC/ 1000, color='orange', label = 'Filter')
-        plt.plot(FREQUENCY_MAP, powerOfADC / 1000, color='orange', label = 'Filter')
-        plt.legend(ncol = 3, frameon=False)
-        plt.savefig('img/componentBaseBandPowerUplink.svg', dpi=500)
-
-    else:
-        fig = plt.figure(figsize = (15*cm, 7.5*cm))
-        plt.ylabel('Power / W')
-        plt.xlabel('Frequency / MHz')
-        total_power = (np.array(powerOfIQ) + np.array(powerOfATTEN) + np.array(powerOfBUFF) + np.array(powerOfFVC) + np.array(powerOfOFEVC) + np.array(powerOfFM) + np.array(powerOfCLK) + np.array(powerOfDC) + np.array(powerOfADC)) / 1000
-        plt.plot(FREQUENCY_MAP, total_power + calculateLeakagePower(total_power), color='black')
-        plt.savefig('img/totalBaseBandPowerUplink.svg', dpi=500)
+#totalPowerBaseBand()
     
 def reproduceFigure2():
     
@@ -153,69 +52,52 @@ def reproduceFigure2():
     power_frequency = [398.8, 457.02, 490.23, 519.26, 531.66, 560.49, 601.70, 659.45, 729.51, 820.09, 865.35, 926.98, 1009.12, 1095.36, 1165.1, 1202.02, 1263.70, 1300.6]
     
     interpolated_data = interp1d(load_frequency, power_frequency)
+    LOAD = range(0,99)
     power_frequency_interpolated = [interpolated_data(i) for i in LOAD]
     
     load_time = []
     power_time = []
+    total_power_PA = []
+    total_power_BB = []
+    total_power_RF = []
+    total_power_Oh = []
     
     fig = plt.figure(figsize = (15*cm, 15*cm))
     
-    # =============================================================================
-    # Baseband power consumption
-    # =============================================================================
+    for l in LOAD:
+        parameters = {'BW' : 10, 'Ant' : 4, 'M' : 3/4, 'R' : 2, 'dt' : l, 'df' : 100}
+        BB, RF, PA, Oh = returnTotalPower(parameters)
+        total_power_PA.append(PA)
+        total_power_BB.append(BB)
+        total_power_RF.append(RF)
+        total_power_Oh.append(Oh)
+        
     
-    powerOfFDUp, powerOfCPUUp, powerOfFECUp, powerOfFD_NLUp, powerOfOFDMUp, powerOfFILETERUp = returnFigure2EnergyUplink(LOAD)
-    powerOfFDDown, powerOfCPUDown, powerOfFECDown, powerOfFD_NLDown, powerOfOFDMDown, powerOfFILETERDown, powerOfDPDDown = returnFigure2EnergyDownlink(LOAD)
-   
-    total_power_BB_up = powerOfFDUp + powerOfCPUUp + powerOfFECUp + powerOfFD_NLUp + powerOfOFDMUp + powerOfFILETERUp 
-    total_power_BB_down = powerOfFDDown + powerOfCPUDown + powerOfFECDown + powerOfFD_NLDown + powerOfOFDMDown + powerOfFILETERDown + powerOfDPDDown 
-    total_power_BB = np.array((total_power_BB_up + total_power_BB_down) + calculateLeakagePower(total_power_BB_up + total_power_BB_down))
+    total_power_PA = np.array(total_power_PA)
+    total_power_BB = np.array(total_power_BB)
+    total_power_RF = np.array(total_power_RF)
+    total_power_Oh = np.array(total_power_Oh)
     
-    # =============================================================================
-    # RF power consumption
-    # =============================================================================
-    
-    powerOfIQ_TRANSMITTER, powerOfATTEN_TRANSMITTER, powerOfBUFF_TRANSMITTER, powerOfFVC_TRANSMITTER, powerOfOFEVC_TRANSMITTER, powerOfFM_TRANSMITTER, powerOfCLK_TRANSMITTER, powerOfDC_TRANSMITTER, powerOfADC_TRANSMITTER = returnFigure2RFTransmitterEnergy(LOAD) 
-    powerOfLNA1_RECEIVER, powerOfATTEN_RECEIVER, powerOfLNA2_RECEIVER, powerOfDUAL_MIXER_RECEIVER, powerOfOVGA_RECEIVER, powerOfCLKGEN_RECEIVER, powerOfADC_RECEIVER = returnFigure2RFReceiverEnergy(LOAD)  
-    RF_TRANSMITTER_TOTAL = np.array(powerOfIQ_TRANSMITTER + powerOfATTEN_TRANSMITTER + powerOfBUFF_TRANSMITTER + powerOfFVC_TRANSMITTER + powerOfOFEVC_TRANSMITTER + powerOfFM_TRANSMITTER + powerOfCLK_TRANSMITTER + powerOfDC_TRANSMITTER + powerOfADC_TRANSMITTER) / 1000
-    RF_RECEIVER_TOTAL = np.array(powerOfLNA1_RECEIVER + powerOfATTEN_RECEIVER + powerOfLNA2_RECEIVER + powerOfDUAL_MIXER_RECEIVER + powerOfOVGA_RECEIVER + powerOfCLKGEN_RECEIVER + powerOfADC_RECEIVER) / 1000
-    total_power_RF = RF_TRANSMITTER_TOTAL + RF_RECEIVER_TOTAL
-    
-    
-    # =============================================================================
-    # Power amplifier power consumption    
-    # =============================================================================
-    
-    total_power_PA = [float(i) for i in open('powerAmplifier.txt', 'r').read().split('\n')]
-    
-    # =============================================================================
-    # Overhead power coonsumption
-    # =============================================================================
-
-    total_power_Oh = returnOverHead(total_power_BB, total_power_RF, total_power_PA)
-
-    total_power = total_power_BB + total_power_PA + total_power_RF + total_power_Oh
+    total_power_BB += calculateLeakagePower(np.array(total_power_BB))
     
     plt.plot(LOAD, total_power_PA, color = CB91_Green, label = 'Power amplifier')
     plt.plot(LOAD, total_power_BB, color = CB91_Blue, label = 'Baseband')
     plt.plot(LOAD, total_power_RF, color = CB91_Amber, label = 'Radio')
-    
     plt.plot(LOAD, total_power_Oh, color = CB91_Violet, label = 'Overhead')
+    plt.plot(LOAD, total_power_PA + total_power_BB + total_power_RF + total_power_Oh, color='black', linestyle = 'dotted', label = 'total')
+
+    
     plt.plot(load_frequency, power_frequency, color = 'red', label = '(Desset et al)')
-    plt.plot(LOAD, total_power + calculateLeakagePower(total_power), color='black', linestyle = 'dotted', label = 'total')
-   
+       
     plt.ylabel('Power / W')
     plt.xlabel('Load / %')
     plt.legend()
     plt.savefig('img/totalBaseBandPower.svg', dpi=500)
     
 
-totalPowerBaseBand()   
-#reproduceFigure2()
-    
 
     
-    
+reproduceFigure2()
     
     
     
